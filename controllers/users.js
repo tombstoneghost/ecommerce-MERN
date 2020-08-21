6c6271f6caa1f05dc3ab41833e93575acf473503
tombstoneGhost <simardeepsngh1@gmail.com>
2020-08-21 22:34:42 +0530

    User Signup, Signin and Signout Added

---
+++ controllers/users.js
@@ -0,0 +1,55 @@
+const User = require('../models/users');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+
+const {errorHandler} = require('../helpers/dbErrorHandlers');
+
+exports.signup = (req, res) => {
+    console.log("req.body", req.body);
+    const user = new User(req.body);
+    user.save((err, user) => {
+        if(err) {
+            return res.status(400).json({
+                err: errorHandler(err)
+            })
+        }
+        user.salt = undefined
+        user.hashedPassword = undefined
+        res.json({
+            user
+        });
+    });
+};
+
+exports.signin = (req, res) => {
+    //Find the user based on email
+    const {email, password} = req.body
+    User.findOne({email}, (err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist. Please signup'
+            })
+        }
+        //If user is found make sure that email and password match
+        //Create Authenticate method in user model
+
+        if(!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password don't match"
+            })
+        }
+
+        //Generate a signed token with User Id and Secret
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
+        //Presist the token as 't' in cookie with expiry date
+        res.cookie('t', token, {expire: new Date() + 9999})
+        //Return Response with User and Token to Frontend Client
+        const {_id, name, email, role} = user
+        return res.json({token, user: {_id, email, name, role } });
+    });  
+};
+
+exports.signout = (req, res) => {
+    res.clearCookie('t');
+    res.json({message: "Signout Success"});
+};
---
+++ models/users.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const {v1: uuidv1} = require('uuid');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        require: true,
+        maxlength: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        require: true,
+        unique: true
+    },
+    hashedPassword: {
+        type: String,
+        require: true
+    },
+    about: {
+        type: String,
+        trim: true
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
+    },
+    history: {
+        type: Array,
+        default: []
+    }
+}, {
+    timestamps: true
+});
+
+//virtual field
+
+userSchema.virtual('password')
+.set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashedPassword = this.encryptPassword(password);
+})
+.get(function() {
+    return this._password;
+})
+
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) == this.hashedPassword;
+    },
+
+    encryptPassword: function(password) {
+        if(!password) return ' ';
+
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    }
+}
+
+module.exports = mongoose.model("User", userSchema);
