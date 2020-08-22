830aef92e1353111297c702b0372b80f281aa97f
tombstoneGhost <simardeepsngh1@gmail.com>
2020-08-22 16:26:17 +0530

    Sending Products

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,23 @@ exports.userById = (req, res, next, id) => {
         next();
     });
 };
+
+exports.read = (req, res) => {
+    req.profile.hashedPassword = undefined;
+    req.profile.salt = undefined;
+
+    return res.json(req.profile);
+};
+
+exports.update = (req, res) => {
+    User.findOneAndUpdate({_id: req.profile._id}, {$set: req.body}, {new: true}), (err, user) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'You are not authorized to permison this action'
+            });
+        }
+        user.hashedPassword = undefined;
+        user.salt = undefined;
+        res.json(user);
+    };
+};
