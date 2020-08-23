84a6ffae2a2470948368157afab850559126e486
tombstoneGhost <simardeepsngh1@gmail.com>
2020-08-23 20:05:18 +0530

    User Signup and Signin

--- ecommerce-front/src/core/Menu.js
+++ ecommerce-front/src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import {signout, isAuthenricated} from '../auth/index';
 
 const isActive = (history, path) => {
     if(history.location.pathname === path) {
@@ -16,12 +17,25 @@ const Menu = ({history}) => {
                 <li className="nav-item">
                     <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
                 </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">Signin</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
-                </li>
+                {!isAuthenricated() && (
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">Signin</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
+                        </li>
+                    </>
+                )}
+                
+                {isAuthenricated() && (
+                    <li className="nav-item">
+                        <span className="nav-link" style={{cursor: 'pointer', color: '#ffffff'}} onClick={() => signout(() => {
+                            history.push('/');
+                            })
+                        }>Signout</span>
+                    </li>
+                )}
             </ul>
         </div>
     );
